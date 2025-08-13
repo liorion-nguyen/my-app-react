00c3d04b574df327813d23f82da3a571991ce77b
Liorion Nguyen <liorion.nguyen@gmail.com>
2025-08-13 23:32:40 +0700

    feat: project

--- src/features/auth/login/index.tsx
+++ src/features/auth/login/index.tsx
@@ -1,21 +1,19 @@
 import { User } from "src/shared/types/user.type";
 import { Status } from "../../../shared/enums/user.enum";
-import { formatDateYYDDMM } from "../../../shared/utils/formatDate";
 import { useEffect, useState } from "react";
 
 export default function Login() {
     const status: Status = Status.APPROVED;
-    const [users, setUsers] = useState<User[]>([]);
+    const [users, setUsers] = useState<User[]>([]); // init users state as an empty array 
     const getUser = async() => {
         const response = await fetch('https://jsonplaceholder.typicode.com/users');
         const data = await response.json();
-        setUsers(data);
+        setUsers(data); // Update the state with the fetched users
     }
 
-    useEffect(() => {
+    useEffect(() => { // Fetch users when the component mounts
         getUser();
     }, []);
-    
     return (
         // jsx
         <div style={{ 
